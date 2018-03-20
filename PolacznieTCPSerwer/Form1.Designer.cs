19e45ad273156ea29f3d85927a0b83f1dd0513fa
Madnarth <corwyn.blade@gmail.com>
2018-03-20 20:55:50 +0100

    Initial commit

---
+++ PolacznieTCPSerwer/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace PolacznieTCPSerwer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bStart = new System.Windows.Forms.Button();
+            this.lbLogs = new System.Windows.Forms.ListBox();
+            this.numPort = new System.Windows.Forms.NumericUpDown();
+            this.tbHostAddress = new System.Windows.Forms.TextBox();
+            this.bStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(271, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Port:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Adres:";
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(109, 176);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(75, 23);
+            this.bStart.TabIndex = 9;
+            this.bStart.Text = "Start";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // lbLogs
+            // 
+            this.lbLogs.FormattingEnabled = true;
+            this.lbLogs.Location = new System.Drawing.Point(12, 73);
+            this.lbLogs.Name = "lbLogs";
+            this.lbLogs.Size = new System.Drawing.Size(360, 95);
+            this.lbLogs.TabIndex = 8;
+            // 
+            // numPort
+            // 
+            this.numPort.Location = new System.Drawing.Point(306, 25);
+            this.numPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numPort.Name = "numPort";
+            this.numPort.Size = new System.Drawing.Size(66, 20);
+            this.numPort.TabIndex = 7;
+            // 
+            // tbHostAddress
+            // 
+            this.tbHostAddress.Location = new System.Drawing.Point(54, 25);
+            this.tbHostAddress.Name = "tbHostAddress";
+            this.tbHostAddress.Size = new System.Drawing.Size(211, 20);
+            this.tbHostAddress.TabIndex = 6;
+            // 
+            // bStop
+            // 
+            this.bStop.Enabled = false;
+            this.bStop.Location = new System.Drawing.Point(190, 176);
+            this.bStop.Name = "bStop";
+            this.bStop.Size = new System.Drawing.Size(75, 23);
+            this.bStop.TabIndex = 12;
+            this.bStop.Text = "Stop";
+            this.bStop.UseVisualStyleBackColor = true;
+            this.bStop.Click += new System.EventHandler(this.bStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 211);
+            this.Controls.Add(this.bStop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bStart);
+            this.Controls.Add(this.lbLogs);
+            this.Controls.Add(this.numPort);
+            this.Controls.Add(this.tbHostAddress);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.ListBox lbLogs;
+        private System.Windows.Forms.NumericUpDown numPort;
+        private System.Windows.Forms.TextBox tbHostAddress;
+        private System.Windows.Forms.Button bStop;
+    }
+}
+
---
+++ PolacznieTCPSerwer/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PolacznieTCPSerwer
+{
+    public partial class Form1 : Form
+    {
+        //pola prywatne
+        private TcpListener serwer;
+        private TcpClient klient;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            {
+                IPAddress adresIP;
+                try
+                {
+                    adresIP = IPAddress.Parse(tbHostAddress.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Błędny format adresu IP!", "Błąd");
+                    tbHostAddress.Text = String.Empty;
+                    return;
+                }
+
+                int port = System.Convert.ToInt16(numPort.Value);
+                try
+                {
+                    serwer = new TcpListener(adresIP, port);
+                    serwer.Start();
+                    klient = serwer.AcceptTcpClient();
+                    IPEndPoint IP = (IPEndPoint)klient.Client.RemoteEndPoint;
+                    lbLogs.Items.Add("[" + IP.ToString() + "] :Nawiązano połączenie");
+                    lbLogs.Items.Add("Nawiązano połączenie");
+                    bStart.Enabled = false;
+                    bStop.Enabled = true;
+                    klient.Close();
+                    serwer.Stop();
+                }
+                catch (Exception ex)
+                {
+                    lbLogs.Items.Add("Błąd inicjacji serwera!");
+                    MessageBox.Show(ex.ToString(), "Błąd");
+                }
+            }
+        }
+
+        private void bStop_Click(object sender, EventArgs e)
+        {
+            serwer.Stop();
+            klient.Close();
+            lbLogs.Items.Add("Zakończono pracę serwera ...");
+            bStart.Enabled = true;
+            bStop.Enabled = false;
+        }
+    }
+}
